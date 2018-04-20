162afad723d9ab10e1997943d2c316af3f3e8fd1
Maley1993 <maley1993@gmail.com>
2018-04-20 12:18:55 -0700

    corrections made to views and a few changes to config files.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'welcome/about'
-  
+
   root 'welcome#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  default_url_options host: "localhost"
 end
